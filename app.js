d7cb15813f2b2c7d868eb225c1533ccb70095d78
Michael McDonough <mikemac206@gmail.com>
2017-05-02 15:20:37 +0200

    updated address

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ $( document ).ready(function() {
   for (var i = 0; i < candidateNames.length; i++) {
     let name = candidateNames[i];
     
-    ethervote.totalVotesFor(5361726168).then(function(v) {
+    ethervote.totalVotesFor(0x5361726168000000000000000000000000000000000000000000000000000000).then(function(v) {
       $("#" + candidates[name]).html(v.toString());
     }); 
     
